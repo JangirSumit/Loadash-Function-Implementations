204dd5a30cf5bd8ddd6f3427fa7ab936482a8636
Sumit Jangir (XSUMJ) <XSUMJ@saxobank.com>
2019-06-25 17:42:32 +0530

    _.dropRight(array, [n = 1])

--- array.js
+++ array.js
@@ -17,8 +17,11 @@ function chunk(arr, size = 1) {
     return result;
 }
 
-console.log(chunk(['a', 'b', 'c', 'd'], 2)) // [['a', 'b'], ['c', 'd']]
-console.log(chunk(['a', 'b', 'c', 'd'], 3)) // [['a', 'b', 'c'], ['d']]
+console.log(chunk(['a', 'b', 'c', 'd'], 2))
+// => [['a', 'b'], ['c', 'd']]
+
+console.log(chunk(['a', 'b', 'c', 'd'], 3))
+// => [['a', 'b', 'c'], ['d']]
 
 //////////////////////////////////////////////////////////////////////////////////////
 
@@ -29,7 +32,8 @@ function compact(arr) {
     return arr.filter((ele) => !!ele);
 }
 
-console.log(compact([0, 1, false, 2, '', 3])); // [1, 2, 3]
+console.log(compact([0, 1, false, 2, '', 3]));
+// => [1, 2, 3]
 
 /////////////////////////////////////////////////////////////////////////////////////
 
@@ -42,11 +46,15 @@ function concat(arr, ...values) {
 
 var array = [1];
 var other = concat(array, 2, [3], [[4]]);
-console.log(other); // [1, 2, 3, Array(1)]
+console.log(other);
+// => [1, 2, 3, [4]]
+
+console.log(array);
+// => [1]
 
 /////////////////////////////////////////////////////////////////////////////////////
 
-// 4. _.difference(array, [values])
+// 4: _.difference(array, [values])
 // Creates an array of array values not included in the other given arrays using SameValueZero for equality comparisons. 
 // The order and references of result values are determined by the first array.
 
@@ -55,3 +63,46 @@ function difference(arr1, arr2) {
 }
 
 console.log(difference([2, 1], [2, 3]));
+// => [1]
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+// 5. _.drop(array, [n=1])
+// Creates a slice of array with n elements dropped from the beginning.
+
+function drop(arr, n = 1) {
+    return arr.slice(n);
+}
+
+console.log(drop([1, 2, 3]));
+// => [2, 3]
+
+console.log(drop([1, 2, 3], 2));
+// => [3]
+
+console.log(drop([1, 2, 3], 5));
+// => []
+
+console.log(drop([1, 2, 3], 0));
+// => [1, 2, 3]
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+// 6: _.dropRight(array, [n = 1])
+// Creates a slice of array with n elements dropped from the end.
+
+function dropRight(arr, n = 1) {
+    return arr.slice(0, -n);
+}
+
+console.log(drop([1, 2, 3]));
+// => [1, 2]
+
+console.log(drop([1, 2, 3], 2));
+// => [1]
+
+console.log(drop([1, 2, 3], 5));
+// => []
+
+console.log(drop([1, 2, 3], 0));
+// => []
