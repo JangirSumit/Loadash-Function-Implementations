e667c3fbda60d37d75c9b23d3a539cb266a24e8b
Sumit Jangir (XSUMJ) <XSUMJ@saxobank.com>
2019-06-25 16:18:01 +0530

    _.compact(array)

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // 1: _.chunk(array, [size=1])
+// Creates an array of elements split into groups the length of size. 
+// If array can't be split evenly, the final chunk will be the remaining elements.
 
 function chunk(arr, size = 1) {
     let result = [];
@@ -14,3 +16,14 @@ function chunk(arr, size = 1) {
 
 console.log(chunk(['a', 'b', 'c', 'd'], 2)) // [['a', 'b'], ['c', 'd']]
 console.log(chunk(['a', 'b', 'c', 'd'], 3)) // [['a', 'b', 'c'], ['d']]
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+// 2: _.compact(array)
+// Creates an array with all falsey values removed. The values false, null, 0, "", undefined, and NaN are falsey.
+
+function compact(arr) {
+    return arr.filter((ele) => !!ele);
+}
+
+console.log(compact([0, 1, false, 2, '', 3]));
